72ebdc4aadffe80302f521bcbd2b3d32e7c3c62b
nikhilleo <ramrakhyani94@gmail.com>
2021-02-01 21:35:55 +0530

    Slip2 Solved

---
+++ Slip2/q1.java
@@ -0,0 +1,35 @@
+// Write a java program to accept n names of cites from user and display them in descending
+// order.
+
+import java.lang.*;
+import java.util.*;
+
+public class q1 { 
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number Of Cities To Enter:");
+        n = sc.nextInt();
+        String str[] = new String[n];
+        System.out.println("Enter Cities:");
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < i ; j++)
+            {
+                if(str[i].compareTo(str[j]) < 0)
+                {
+                    String temp = str[i];
+                    str[i] = str[j];
+                    str[j] = temp;
+                }
+            }
+        }
+        System.out.println("Cities In Decending Order Are:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(str[i]);
+        }
+    }
+}
